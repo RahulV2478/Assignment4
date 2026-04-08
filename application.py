9daea6f57fe8704dbd44c91977f1de2db971efb1
Rahul Vuggumudi <rahulv@utexas.edu>
2026-04-08 00:02:16 -0500

    uploaded wrong ami

--- application.py
+++ application.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    value = os.environ.get("MY_ENV_VAR", "")
-    return value
+    env_var = os.environ.get("MY_ENV_VAR")
+    return env_var
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
